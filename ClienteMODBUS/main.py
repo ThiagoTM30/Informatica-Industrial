742bd8b081f60a081b7d130c0673183e2c3db385
JuliaaPaivaa <julia.paiva@estudante.ufjf.br>
2025-03-10 08:53:15 -0300

    Add files via upload

--- ClienteMODBUS/main.py
+++ ClienteMODBUS/main.py
@@ -1,5 +1,20 @@
-from cliente import ClienteMODBUS
+from kivy.app import App
+from mainwidget import MainWidget
+from kivy.lang.builder import Builder
 
-print('comecei')
-c = ClienteMODBUS('localhost',502)
-c.readData()
+
+class MainApp(App):
+    """
+    classe com o aplicativo
+    """
+    def build(self):
+        """
+        Metodo que gera o app com base no widget
+        """
+        self._widget = MainWidget()
+        return self._widget
+    
+if __name__ == '__main__':
+    
+    Builder.load_string(open("Trabalhoinf/mainwidget.kv",encoding="utf-8").read(),rulesonly=True)
+    MainApp().run()
